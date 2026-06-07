5998a8f355d1e0049f0adc42a595dde3cd0eea57
mohammadtonor <mtonor1368@gmail.com>
2026-06-07 04:02:16 +0330

    add shadcn ui to my project

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,19 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
 export default function Home() {
-  return <h1>Hello World!</h1>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center gap-6 p-8">
+      <h1 className="text-4xl font-bold">Hello World!</h1>
+      <Input />
+      <Button variant="primary" size="lg">primary</Button>
+      <Button variant="secondary">secondary</Button>
+      <Button variant="destructive">destructive</Button>
+      <Button variant="ghost">ghost</Button>
+      <Button variant="primary">default</Button>
+      <Button variant="muted">Muted</Button>
+      <Button variant="teritary">Teritary</Button>
+    </main>
+  );
 }
+   
